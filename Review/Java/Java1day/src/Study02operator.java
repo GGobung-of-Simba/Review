a501a52b4d0c4d632c36636017e17c31f94c755d
J.Lim HOME <j.lim9304@gamil.com>
2024-11-25 00:43:19 +0900

    연산자

---
+++ Review/Java/Java1day/src/Study02operator.java
@@ -0,0 +1,20 @@
+public class Study02operator {
+    public static void main (String[] arg){
+        int a = 5;
+        int b = 2;
+
+        int plus = (a+b);
+        int minus = (a-b);
+        int mul = (a * b);
+        int de = (a / b);
+        int rest = (a % b);
+        
+        System.out.println(plus);
+        System.out.println(minus);
+        System.out.println(mul);
+        System.out.println(de);
+        System.out.println(rest);
+        
+    }
+    
+}

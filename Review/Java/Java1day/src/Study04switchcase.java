4fda55ca3bdb400a37951c73649bfeb5f7fe9009
J.Lim HOME <j.lim9304@gamil.com>
2024-11-25 01:01:23 +0900

    switch case문

---
+++ Review/Java/Java1day/src/Study04switchcase.java
@@ -0,0 +1,28 @@
+public class Study04switchcase {
+    public static void main(String[] arg){
+        int a = 3;
+        switch(a){
+            case 1 :
+            System.out.println("one");
+            break;
+            case 2 :
+            System.out.println("two");
+            break;
+            case 3 :
+            System.out.println("three");
+            break;
+            case 4 :
+            System.out.println("four");
+            break;
+            case 5 :
+            System.out.println("five");
+            break;
+            case 6 :
+            System.out.println("six");
+            break;
+            default:
+            System.out.println("the other");
+            break;
+        }
+    }
+}

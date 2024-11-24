c51455028991162c74f6c67a83a25300dc8d2067
J.Lim HOME <j.lim9304@gamil.com>
2024-11-25 01:18:24 +0900

    swich문 만들어보기

---
+++ Review/Java/Java1day/src/Studyalone02.java
@@ -0,0 +1,27 @@
+public class Studyalone02 {
+    public static void main(String[] arg){
+        int 절도 = 1;
+        int 사기 = 2;
+        int 살인 = 3;
+        int 폭행 = 4;
+
+        switch (폭행) {
+            case 1 :
+            System.out.println("1년 이하의 징역");
+            break;
+            case 2 :
+            System.out.println("2년 이하의 징역");
+            break;
+            case 3 :
+            System.out.println("3년 이하의 징역");
+            break;
+            case 4 :
+            System.out.println("4년 이하의 징역");
+            break;
+            default:
+            System.out.println("그건 잘 모르겠네");
+            break;
+        }
+    }
+}
+    
